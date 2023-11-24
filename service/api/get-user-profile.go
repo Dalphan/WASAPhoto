59dd434fb4f595b5314124a52836c01a97f38747
Dalphan0 <danyventurini25@gmail.com>
2023-11-24 11:25:21 +0100

    Fixed get-user-profile.go

--- service/api/get-user-profile.go
+++ service/api/get-user-profile.go
@@ -1,9 +1,11 @@
 package api
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 
+	"github.com/Dalphan/WASAPhoto/service/database"
 	"github.com/Dalphan/WASAPhoto/service/utils"
 	"github.com/julienschmidt/httprouter"
 )
@@ -22,14 +24,14 @@ func (rt *_router) getUserProfile(w http.ResponseWriter, r *http.Request, ps htt
 		//other way
 		//w.WriteHeader(http.StatusNotAcceptable)
 		//fmt.Fprint(w, err.Error())
-	} else { 
+	} else {
 		//Lo username è valido, cerca nel database
-		user, res, err := db.FindUserByUsername(username)
+		user, res, err := rt.db.FindUserByUsername(username)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-	
+
 		// Se non ha trovato niente, allora ritorna codice 404 Not Found
 		if res == database.NO_ROWS {
 			http.Error(w, utils.ErrUserNotFound.Error(), http.StatusNotFound)
