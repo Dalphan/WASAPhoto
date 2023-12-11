c0214af97cd3b3cb5f5aae298e35b875285de9da
Dalphan <danyventurini25@gmail.com>
2023-12-11 20:33:34 +0100

    Added transaction to the ban user

--- service/database/ban-db.go
+++ service/database/ban-db.go
@@ -1,14 +1,36 @@
 package database
 
+// Banning a user involves removing their follow status from both users
+// and deleting likes and comments made by the banned user on the photos
+// of the user who is implementing the ban.
 func (db *appdbimpl) BanUser(uid int, bid int) (int, error) {
-	_, err := db.c.Exec(`	INSERT INTO Bans
-							VALUES (?, ?)`, uid, bid)
-
-	if res := checkResults(err); res != SUCCESS {
-		return res, err
-	} else {
-		return res, nil
+	tx, err := db.c.Begin()
+	if err != nil {
+		return ERROR, err
 	}
+
+	_, err = tx.Exec(`	INSERT INTO Bans
+						VALUES (?, ?)`, uid, bid)
+
+	res := checkResults(err)
+	if res != SUCCESS {
+		tx.Rollback()
+		return res, err
+	}
+	_, err = tx.Exec(`	DELETE FROM Followings
+						WHERE (UID = ? AND BannedID)
+						OR (UID = ? and BannedID = ?)`, uid, bid, bid, uid)
+	if err != nil {
+		tx.Rollback()
+		return ERROR, err
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		tx.Rollback()
+		return ERROR, err
+	}
+	return SUCCESS, err
 }
 
 func (db *appdbimpl) UnbanUser(uid int, bid int) (int, error) {
